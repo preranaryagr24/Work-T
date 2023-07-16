bff2e8871166e93ee08e52bcde4601034e439b1d
preranaryagr24 <83573297+preranaryagr24@users.noreply.github.com>
2023-07-16 03:32:37 +0000

    initial design

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-
+from flask import Flask , render_template
+#flask is the module Flask is the class
 app = Flask(__name__)
 
+
+#decoraor
 @app.route("/")
 def hello():
-    return "Hello, World!"
+
+  return render_template("./home.html")
 
 
 print(__name__)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
